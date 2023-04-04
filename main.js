d28050dce1f93d9864b3db376a901f93c1c3d954
GG <105051795+GGyeet@users.noreply.github.com>
2023-04-04 17:25:34 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+music="";
+function preload(){
+    music=loadSound("music.mp3");
+    }
+    function setup(){
+        canvas=createCanvas(600,500);
+        canvas.center();
+        video=createCapture(VIDEO);
+        video.hide();
+        }
+
+        function draw(){
+            image(video, 0, 0, 500, 600);
+        }
